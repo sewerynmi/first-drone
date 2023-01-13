dd05ebfdbdbb61cffed18d0f06211eb49963f547
Seweryn Michota <sewerynmichota@gmail.com>
2023-01-13 16:57:00 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, "hello\n")
+}
+
+func test(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "test seems to work fine\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
+	http.HandleFunc("/test", test)
+
+    http.ListenAndServe(":8090", nil)
+}
